e68588041cebc6544f596de185d3042e793da4d7
Adnan-Sarkar <adnansarkar111@gmail.com>
2023-11-18 23:46:46 +0600

    responsive: NavbarTop component

--- src/components/Navbar/NavbarTop.jsx
+++ src/components/Navbar/NavbarTop.jsx
@@ -1,14 +1,14 @@
 const NavbarTop = () => {
   return (
     <div className="container">
-      <div className="flex items-center py-[32px]">
+      <div className="flex items-center sm:flex-row flex-col gap-y-5 py-[32px]">
         <div>
           <picture>
             <img src="./logo.png" alt="OREBI" />
           </picture>
         </div>
         <div className="w-full">
-          <ul className="flex items-center justify-center gap-x-[40px]">
+          <ul className="flex items-center justify-center sm:gap-x-[40px] gap-x-[20px]">
             <li>
               <a className="text-primary text-[14px] font-bold" href="#">
                 Shop
